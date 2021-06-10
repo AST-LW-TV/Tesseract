21fa2da692f885a57dc8e205b7d66bf3ffcb8e26
niranjan2055 <niranjany@testvagrant.com>
2021-06-10 08:16:49 +0530

    Update Details.java

--- parkingDesignProblem/src/com/testVagrant/Details.java
+++ parkingDesignProblem/src/com/testVagrant/Details.java
@@ -4,15 +4,14 @@ import java.util.Scanner;
 
 public class Details {
 
-    private String[] detailToBeNoted=new String[4];
+    private String[] detailToBeNoted=new String[3];
     private String[] inputs={"Car Registration Number",
-                            "Time of Entry",
-                             "Vehicle Type",
+                              "Vehicle Type",
                               "Purpose of Visit"};
     Scanner input=new Scanner(System.in);
 
     public String[] returnDetails(){
-        for(int i=0;i<4;i++){
+        for(int i=0;i<3;i++){
             System.out.println("Enter the "+this.inputs[i]);
             this.detailToBeNoted[i]=input.nextLine();
         }
